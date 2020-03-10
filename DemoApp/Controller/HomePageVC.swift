51278f8b649e2804af47225de36c54baace07536
ARIFJEZIR <35096139+ARIFJEZIR@users.noreply.github.com>
2020-03-10 23:50:56 +0530

    Update HomePageVC.swift

--- DemoApp/Controller/HomePageVC.swift
+++ DemoApp/Controller/HomePageVC.swift
@@ -34,7 +34,39 @@ class HomePageVC: UIViewController {
             dialogMessage.addAction(cancel)
             self.present(dialogMessage, animated: true, completion: nil)
         }
-    
+    func getCurrentExperience() {
+        self.isLoading = true
+        let params = [
+            "page_no": self.pgNo,
+            ] as [String : Any]
+        homeVM.getExperience(params:params, isProgress: isProgress) { (status) in
+            if status {
+                if self.pgNo == 1{
+                    self.experience = self.homeVM.experience
+                }
+                else{
+                     self.experience += self.homeVM.experience
+                }
+                self.tableView.reloadData()
+                    self.tableView.tableFooterView = UIView()
+                if !self.experience.isEmpty &&  self.pgNo == 1{
+                    self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+                }
+//                else{
+//                    self.tableView.scrollToBottom()
+//                }
+                if self.pgNo <= self.homeVM.home.totalPage {
+                                           self.pgNo += 1
+                                           DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                               self.isLoading = false
+                                           }
+                                       }else{
+                                           self.isLoading = true
+                                       }
+                }
+            
+            
+        }
     func getHomePageData(){
          let header = ["x-auth-token":USERDEFAULTS.getAuthToken()]
         APIManager.shared.getRequest(stringApi: HomeAPI.homeURL, headers: header){
@@ -49,6 +81,31 @@ class HomePageVC: UIViewController {
         }
         
         
+    }
+    
+    extension HomePageVC {
+        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if (roundf(Float(scrollView.contentOffset.y)) == roundf(Float(scrollView.contentSize.height) - Float(scrollView.frame.size.height))) {
+            if(!isLoading){
+                let activityindicator = UIActivityIndicatorView(style: .gray)
+                let footerview = UIView(frame: CGRect(x: 0, y: 0, width: self.view.width, height: 40))
+                activityindicator.center = footerview.center
+                footerview.addSubview(activityindicator)
+                activityindicator.color = UIColor.white
+                activityindicator.startAnimating()
+                self.tableView.tableFooterView = footerview
+                self.tableView.tableFooterView!.center = footerview.center
+                self.tableView.scrollRectToVisible(self.tableView.convert((self.tableView.tableFooterView?.bounds)!, from: self.tableView.tableFooterView) , animated: true)
+                if self.pgNo <= self.homeVM.home.totalPage {
+                    isProgress = false
+                } else {
+                     activityindicator.stopAnimating()
+                }
+               
+            }
+        }
+        
+    }
     }
 
 }
